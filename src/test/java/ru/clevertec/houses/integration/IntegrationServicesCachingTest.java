f803f81573a20ada2ec265c3ef072d293020f46a
Kihtenko Olga <kiok.ko1811@gmail.com>
2024-02-01 20:23:50 +0300

    docs: README.md

--- src/test/java/ru/clevertec/houses/integration/IntegrationServicesCachingTest.java
+++ src/test/java/ru/clevertec/houses/integration/IntegrationServicesCachingTest.java
@@ -22,7 +22,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
