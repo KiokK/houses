a2d5a0c3899a6aa155bc8e40644c26eab04907c0
Kihtenko Olga <kiok.ko1811@gmail.com>
2024-02-08 00:58:44 +0300

    remove cache-starter

--- cache-starter/src/main/java/ru/clevertec/cachestarter/exeption/CacheInitializationException.java
+++
@@ -1,9 +0,0 @@
-package ru.clevertec.cachestarter.exeption;
-
-public class CacheInitializationException extends RuntimeException {
-
-    public CacheInitializationException(String property) {
-        super(String.format("Error cash-starter application.property: '%s'", property));
-    }
-
-}
--- cache-starter/src/main/java/ru/clevertec/cachestarter/exeption/CacheKeyFieldNotFoundInObjectException.java
+++
@@ -1,9 +0,0 @@
-package ru.clevertec.cachestarter.exeption;
-
-public class CacheKeyFieldNotFoundInObjectException extends NoSuchFieldException {
-
-    public CacheKeyFieldNotFoundInObjectException(String fieldName) {
-        super(String.format("Cacheable object has not field '%s'", fieldName));
-    }
-
-}
