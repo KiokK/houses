63959187ecf60534ba62e94f3bbae217a1a9ca9d
Kihtenko Olga <kiok.ko1811@gmail.com>
2024-02-09 00:05:48 +0300

    feat: cache-starter and exception-handler-starter

---
+++ src/main/java/ru/clevertec/houses/exception/ErrorCode.java
@@ -0,0 +1,6 @@
+package ru.clevertec.houses.exception;
+
+public class ErrorCode {
+    public static final int HOUSE_NOT_FOUND = 4044001;
+    public static final int PERSON_NOT_FOUND = 4044002;
+}
